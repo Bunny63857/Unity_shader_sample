2d610441042a019196d971f3f4bf4de12ed266c9
Shouki Imai <ukiru77@gmail.com>
2020-05-06 21:57:11 +0900

    Add : 頂点がブワッと広がるシェーダー

---
+++ Assets/PointController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointController : MonoBehaviour
+{
+    //MeshFilter meshfilter;
+    void Start()
+    {
+        MeshFilter meshfilter = GetComponent<MeshFilter>();
+        meshfilter.mesh.SetIndices(meshfilter.mesh.GetIndices(0), MeshTopology.Points, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
